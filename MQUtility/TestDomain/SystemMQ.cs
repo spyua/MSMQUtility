c5f81cd0c23c30b6062f9d0fd6699c65c3eb7955
pal2097 <pal2097@gmail.com>
2023-09-15 14:58:41 +0800

    feature:修改專案版本

--- MQUtility/TestDomain/SystemMQ.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestDomain
-{
-    public static class SystemMQ
-    {
-        public const string SndApp = "SndApp";
-        public const string RcvApp = "RcvApp";
-        public const string FunApp = "FuncApp";
-    }
-}
